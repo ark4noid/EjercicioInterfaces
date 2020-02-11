5d5dfa01bd8775dc821f3066543304ee5882caaf
ark4noid <ark4no07@gmail.com>
2020-02-11 11:37:47 +0100

    BLABLA

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace EjerciciosInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Loro paco = new Loro();
+            paco.Hablar();
+        }
+    }
+    interface ICharlatan
+    {
+        public void Hablar();
+    }
+    abstract class Animal
+    {
+
+
+    }
+    class Loro : Animal, ICharlatan
+    {
+        public void Hablar()
+        {
+            Console.WriteLine("BLA BLA, SOY UN LORO, BLA BLA");
+        }
+
+    }
+    class Delfin : Animal
+    {
+
+    }
+    class Tortuga : Animal
+    {
+
+    }
+}
